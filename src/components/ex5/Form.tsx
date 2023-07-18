9b1e9930d30b0b1b27c8f4b575919b9f175d607a
munpro2002 <79945464+munpro2002@users.noreply.github.com>
2023-07-18 18:06:02 +0700

    change to useState ex5

--- src/components/ex5/Form.tsx
+++ src/components/ex5/Form.tsx
@@ -1,25 +1,39 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import styles from "./Form.module.scss";
 
 const RegisterForm = () => {
-  const firstNameRef = useRef<HTMLInputElement>(null);
-  const lastNameRef = useRef<HTMLInputElement>(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const submitButtonHandler = (e: any) => {
     e.preventDefault();
 
-    let firstName = firstNameRef.current?.value;
-    let lastName = lastNameRef.current?.value;
-
     alert(`Hello ${lastName} ${firstName}`);
 
-    e.target.reset();
+    setFirstName("");
+    setLastName("");
   };
 
   return (
     <form onSubmit={submitButtonHandler} className={styles.container}>
-      <input ref={firstNameRef} type="text" placeholder="First Name" required />
-      <input ref={lastNameRef} type="text" placeholder="Last Name" required />
+      <input
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+        type="text"
+        placeholder="First Name"
+        required
+      />
+      <input
+        value={lastName}
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+        type="text"
+        placeholder="Last Name"
+        required
+      />
       <button className={styles["submit-btn"]}>Greet Me</button>
     </form>
   );
